a47b609afba8fdb9a94136e3250678f51a9c16f1
dannyganza <kingdanny@Dannys-Air.hitronhub.home>
2023-04-18 17:01:14 -0600

    added values

---
+++ src/components/FunComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="change_color">Click me</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+ methods: {
+    change_color: function (){
+        document.body [`style`][`backgroundColor`] = `green`
+    }
+ }
+ }
+</script>
+
+<style scoped>
+/* add your styles here */
+</style>
+
